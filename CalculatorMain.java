0b5314c52f029632ead154ad17cfadba0ee5d131
shrikant-23 <dholeshrikant23@gmail.com>
2022-07-26 13:33:06 +0530

    Commit in newfeatures branch

--- CalculatorMain.java
+++ CalculatorMain.java
@@ -8,16 +8,16 @@ public class CalculatorMain {
 	
 	public static void main(String[] args) {
 		Scanner inputnum= new Scanner(System.in);
-		int result;
+		double result;
 		
-		System.out.println("Choose an operator: add, sub, mult, or div");
+		System.out.println("Choose an operator: add, sub, mult, div, percent");
 	   String  operator = inputnum.next();
 	    
 		System.out.println("Enter First Number");
-		int num1= inputnum.nextInt();
+		double num1= inputnum.nextDouble();
 		
 		System.out.println("Enter Second Number");
-		int num2= inputnum.nextInt();
+		double num2= inputnum.nextDouble();
 		
 		
 		switch (operator) {
@@ -46,6 +46,12 @@ public class CalculatorMain {
 	        System.out.println(num1 + " / " + num2 + " = " + result);
 	        break;
 
+	        // performs multiplication between numbers
+	      case "percent":
+	        result = (num1 / num2) * 100;
+	        System.out.println(num1 + " percent out of " + num2 + " = " + result);
+	        break;
+	        
 	      default:
 	        System.out.println("Invalid operator!");
 	        break;
